e9afa9513ba4f88d4dbef7348e1d021a5e2e6aa0
Shanker Ram <91639526+reknahs@users.noreply.github.com>
2022-11-09 16:15:33 -0800

    asdf

--- background.js
+++ background.js
@@ -1,6 +1,6 @@
 chrome.tabs.onUpdated.addListener((tabId, tab) => {
     if(tab.url && tab.url.includes("https://poeltl.dunk.town/")) {
-        alert('yo');
+        alert('hello');
         chrome.tabs.sendMessage();
     }
 })
