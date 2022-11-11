4d05beda124459ad638076f39e32e9e5b66852fe
Shanker Ram <91639526+reknahs@users.noreply.github.com>
2022-11-10 18:34:49 -0800

    asdf

--- background.js
+++ background.js
@@ -1,6 +1,7 @@
-chrome.tabs.onActivated.addListener((tabId, tab) => {
-    if(tab.url && tab.url.includes("https://poeltl.dunk.town/")) {
-        alert('hi');
-        chrome.tabs.sendMessage();
-    }
-})
+// chrome.tabs.onActivated.addListener((tabId, tab) => {
+//     if(tab.url && tab.url.includes("https://poeltl.dunk.town/")) {
+//         alert('hi');
+//         chrome.tabs.sendMessage();
+//     }
+// })
+chrome.tabs.onActivated.addListener(onPage);
--- contentScript.js
+++ contentScript.js
@@ -1,8 +1,12 @@
-(() => {
-    chrome.runtime.onMessage.addListener(() => {
-        alert('yo');
-    })
-})();
+// (() => {
+//     chrome.runtime.onMessage.addListener(() => {
+//         alert('yo');
+//     })
+// })();
 // for(let i = 0; i < 10; i++) {
 //     alert('Hi');
 // }
+async function onPage() {
+    console.log('success');
+    alert('success');
+}
