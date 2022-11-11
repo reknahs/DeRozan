9c60eca203e7145451623f9565f08b611942ae84
Shanker Ram <91639526+reknahs@users.noreply.github.com>
2022-11-10 19:16:16 -0800

    asdf

--- contentScript.js
+++ contentScript.js
@@ -1,14 +1,4 @@
-chrome.tabs.onActivated.addListener(moveToFirstPosition);
-
-async function moveToFirstPosition(activeInfo) {
-  try {
-    await chrome.tabs.move(activeInfo.tabId, {index: 0});
-    alert('sdf');
-  } catch (error) {
-    if (error == 'Error: Tabs cannot be edited right now (user may be dragging a tab).') {
-      setTimeout(() => moveToFirstPosition(activeInfo), 50);
-    } else {
-      alert('cuh');
-    }
-  }
-}
+// chrome.runtime.onMessage.addListener((obj) => {
+    
+// });
+console.log('yello');
