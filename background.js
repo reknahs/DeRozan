ddcb7c4a95c8cdc39c431d8c45eb6aafa3d98f4f
Shanker Ram <91639526+reknahs@users.noreply.github.com>
2022-11-11 10:25:39 -0800

    asdf

--- background.js
+++ background.js
@@ -1,8 +1,7 @@
-chrome.tabs.onUpdated.addListener((tabId, tab) => {
-    console.log('one');
-    if(tab.url) console.log('two');
-    if(tab.url && tab.url.includes("https://poeltl.dunk.town/")) {
-      console.log('success');
-      chrome.tabs.sendMessage(tabId);
-    }
-});
+// chrome.tabs.onUpdated.addListener((tabId, tab) => {
+//     if(tab.url) console.log(tab.url);
+//     if(tab.url && tab.url.includes("https://poeltl.dunk.town/")) {
+//       console.log('success');
+//       //chrome.tabs.sendMessage(tabId);
+//     }
+// });
--- contentScript.js
+++ contentScript.js
@@ -1,4 +1,20 @@
-// chrome.runtime.onMessage.addListener((obj) => {
-    
-// });
-// console.log('yello');
+document.addEventListener("visibilitychange", DOMManipulation());
+
+var guessNum;
+var info = [];
+
+function DOMManipulation() {
+    const textElements = document.getElementsByClassName("text");
+    let name = textElements[0].getElementsByClassName("innerText");
+    // guessNum = textElements.length/8;
+    // for(let i = 0; i < textElements.length; i += 8) {
+    //     let row = [];
+    //     row.push(textElements[0].getElementsByClassName("innerText"));
+
+    // }
+    if(guessNum == 0) document.bgColor = "black";
+    if(guessNum == 1) document.bgColor = "darkgray";
+    if(guessNum == 2) document.bgColor = "purple";
+    if(name === "Stephen Curry") document.bgColor = "red";
+
+}
