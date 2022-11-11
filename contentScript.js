8cb42e0673cf41924ef47f9df69b02f7149d4bf9
Shanker Ram <91639526+reknahs@users.noreply.github.com>
2022-11-10 18:55:19 -0800

    asdf

--- contentScript.js
+++ contentScript.js
@@ -8,8 +8,8 @@
 //     console.log('success');
 //     alert('success');
 // }
-chrome.tabs.onUpdated.addListener(onPage);
+// chrome.tabs.onUpdated.addListener(onPage);
 alert('cuh');
-async function onPage(info) {
-    alert('hi');
-}
+// async function onPage(info) {
+//     alert('hi');
+// }
