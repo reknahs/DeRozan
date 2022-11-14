6aefd92722477de092f1ec680414c3665d9a6722
Shanker Ram <91639526+reknahs@users.noreply.github.com>
2022-11-13 22:23:26 -0800

    af

--- webscraping/webscraper.js
+++ webscraping/webscraper.js
@@ -1,5 +1,6 @@
 import fetch from "node-fetch";
 import cheerio from "cheerio";
+import {readFileSync, promises as fsPromises} from 'fs';
 
 var all_links = []
 
@@ -11,18 +12,33 @@ const getRawData = (URL) => {
         });
 }
 
-const parent_URL = "https://www.nba.com/players"
-
 const getAllLinks = async() => {
-    const rawParentData = await getRawData(parent_URL);
-    const parsedParentData = cheerio.load(rawParentData);
-    const nameList = parsedParentData("html body script")[2].children[0].data;
-    let all_ids = nameList.toString().split("PERSON_ID");
-    let len = all_ids.length;
-    for(let i = 1; i < len; i++) {
-        all_links.push("https://www.nba.com/stats/player/"+all_ids[i].slice(2).split(",")[0]+"/career");
+    const contents = readFileSync("webscraping/links.txt", 'utf-8');
+
+    const arr = contents.split(/\r?\n/);
+    for(let i = 0; i < arr.length-1; i++) {
+        console.log(arr[i]);
+        const rawData = await getRawData(arr[i]);
+        const parsedData = cheerio.load(rawData);
+        const age = parsedData("div"); //[1]["children"][0]["data"];
+        for(let j = 0; j < age.length; j++) {
+            for(let k = 0; k < age[j].children.length; k++) {
+                console.log(age[j].children[k]["data"]);
+            }
+        }
+        //console.log(age);
+        break;
+        // console.log(age.slice(age.indexOf("was born on")+12,age.length-1));
     }
-    getPreviousTeamsAndAge()
+
+    // const rawParentData = await getRawData(parent_URL);
+    // const parsedParentData = cheerio.load(rawParentData);
+    // const nameList = parsedParentData("html body script")[2].children[0].data;
+    // let all_ids = nameList.toString().split("PERSON_ID");
+    // let len = all_ids.length;
+    // for(let i = 1; i < len; i++) {
+    //     all_links.push("https://www.nba.com/stats/player/"+all_ids[i].slice(2).split(",")[0]+"/career");
+    // }
 }
 
 const getPreviousTeamsAndAge = async() => {
--- webscraping/webscraper.py
+++ webscraping/webscraper.py
@@ -11,15 +11,19 @@ uClient.close()
 page_soup = soup(page_html, "html.parser")
 new_soup = str(page_soup).replace("null", "'null'").split("}")[246:747]
 players = []
+f = open("webscraping/links.txt", "w")
 for i, v in enumerate(new_soup):
     new_soup[i] = eval(v[1:-17]+"}" if i != 0 else v[12:-17]+"}")
+    name = new_soup[i]["PLAYER_SLUG"].split("-")[1][:5]+new_soup[i]["PLAYER_SLUG"].split("-")[0][:2]
     players.append(new_soup[i]["PLAYER_FIRST_NAME"]+" "+new_soup[i]["PLAYER_LAST_NAME"])
+    link = "https://www.basketball-reference.com/players/"+name[0]+"/"+name+"01.html"
+    f.write(link+"\n")
+f.close()
 
-f = open("data.txt", "w")
+f = open("webscraping/data.txt", "w")
 for i in new_soup:
     f.write(i["PLAYER_FIRST_NAME"]+" "+i["PLAYER_LAST_NAME"]+" ")
     f.write(i["TEAM_ABBREVIATION"]+" ")
     f.write(i["POSITION"]+" "+i["HEIGHT"]+" ")
     f.write(i["JERSEY_NUMBER"]+"\n")
-
 f.close()
